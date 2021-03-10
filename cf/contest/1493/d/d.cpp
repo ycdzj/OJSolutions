2ab402dd29219c55b24bfc231be14346f08fe2b0
ycdzj <yczhao0123@gmail.com>
2021-03-10 19:31:06 +0800

    add cf_1493 DE

---
+++ cf/contest/1493/d/d.cpp
@@ -0,0 +1,257 @@
+// ----------------------Template Start----------------------
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+using namespace std;
+ 
+class Io {
+public:
+    static void read(int &val) { scanf("%d", &val); }
+    static void read(long long &val) { scanf("%lld", &val); }
+    static void read(char *val) { scanf("%s", val); }
+    static void read(string &val) {
+        const int BUFFER_SIZE = 2e6 + 5;
+        static char buffer[BUFFER_SIZE];
+        scanf("%s", buffer);
+        val = buffer;
+    }
+    static void read(vector<int> &val, int n) {
+        val.resize(n);
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &val[i]);
+        }
+    }
+    static void read(vector<long long> &val, int n) {
+        val.resize(n);
+        for (int i = 0; i < n; i++) {
+            scanf("%lld", &val[i]);
+        }
+    }
+ 
+    static void write(int val) { printf("%d\n", val); }
+    static void write(long long val) { printf("%lld\n", val); }
+    static void write(const char *val) { printf("%s\n", val); }
+    static void write(const string &val) { printf("%s\n", val.c_str()); }
+    static void write(const vector<int> &val) {
+        for (int i = 0; i < val.size(); i++) {
+            if (i > 0) {
+                putchar(' ');
+            }
+            printf("%d", val[i]);
+        }
+        putchar('\n');
+    }
+    static void write(const vector<long long> &val) {
+        for (int i = 0; i < val.size(); i++) {
+            if (i > 0) {
+                putchar(' ');
+            }
+            printf("%lld", val[i]);
+        }
+        putchar('\n');
+    }
+};
+ 
+class Mod {
+public:
+    int mod;
+ 
+    explicit Mod(int mod_) : mod(mod_) {}
+ 
+    int add(int a, int b) const { return (a + b) % mod; }
+    int sub(int a, int b) const { return (a + mod - b) % mod; }
+    int mul(long long a, int b) const { return (a * b) % mod; }
+    int pow(long long a, int x) const {
+        int ans = 1;
+        while (x) {
+            if (x & 1) {
+                ans = (ans * a) % mod;
+            }
+            x >>= 1;
+            a = (a * a) % mod;
+        }
+        return ans;
+    }
+    int inv(int x) const {
+        return pow(x, mod - 2);
+    }
+};
+ 
+class Math {
+public:
+    static long long gcd_ex(long long a, long long b, long long &x, long long &y) {
+        if (b == 0) {
+            x = 1;
+            y = 0;
+            return a;
+        }
+        long long ans = gcd_ex(b, a % b, y, x);
+        y -= (a / b) * x;
+        return ans;
+    }
+};
+ 
+class Combine {
+    vector<int> fac;
+    vector<int> inv_fac;
+    Mod mod;
+ 
+public:
+    explicit Combine(int n, Mod mod_) : fac(n), inv_fac(n), mod(mod_) {
+        fac[0] = 1;
+        for (int i = 1; i < n; i++) {
+            fac[i] = mod.mul(fac[i - 1], i);
+        }
+        inv_fac[n - 1] = mod.inv(fac[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            inv_fac[i] = mod.mul(inv_fac[i + 1], i + 1);
+        }
+    }
+    int arrange(int n, int m) {
+        if (n < m) {
+            return 0;
+        }
+        return mod.mul(fac[n], inv_fac[n - m]);
+    }
+    int combine(int n, int m) {
+        if (n < m) {
+            return 0;
+        }
+        return mod.mul(arrange(n, m), inv_fac[m]);
+    }
+};
+ 
+class Dsu {
+    int n;
+    vector<int> f;
+ 
+public:
+    explicit Dsu(int n_) : n(n_), f(n_) {
+        for(int i = 0; i < f.size(); i++) {
+            f[i] = i;
+        }
+    }
+    int find(int x) {
+        if(f[x] != x) {
+            f[x] = find(f[x]);
+        }
+        return f[x];
+    }
+    void merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        f[x] = y;
+    }
+};
+ 
+// ----------------------Template End----------------------
+const int MAXN = 2e5 + 5;
+const int MOD = 1e9 + 7;
+
+Mod mod(MOD);
+
+class Prime {
+public:
+    vector<int> pri;
+    Prime(int n) {
+        pri.resize(n);
+        for(int i = 1; i < n; i++) {
+            pri[i] = i;
+        }
+        for(int i = 2; i < n; i++) {
+            if(pri[i] == i) {
+                for(int j = i * 2; j < n; j += i) {
+                    pri[j] = min(pri[j], i);
+                }
+            }
+        }
+    }
+} prime(MAXN);
+
+class Solver {
+    int n, q;
+    vector<int> a;
+
+    void update_factors(unordered_map<int, int> &factors, int val) {
+        while(val > 1) {
+            int cur = prime.pri[val];
+            factors[cur]++;
+            val /= cur;
+        }
+    }
+
+public:
+    void read() {
+        Io::read(n);
+        Io::read(q);
+        Io::read(a, n);
+    }
+    void solve() {
+        int g = 1;
+        vector<multiset<int>> counts(MAXN);
+        vector<unordered_map<int, int>> factors(n);
+
+        for(int i = 0; i < n; i++) {
+            update_factors(factors[i], a[i]);
+            for(auto pr : factors[i]) {
+                counts[pr.first].insert(pr.second);
+                if(counts[pr.first].size() == n) {
+                    g = mod.mul(g, mod.pow(pr.first, *counts[pr.first].begin()));
+                }
+            }
+        }
+        while(q--) {
+            int i, x;
+            Io::read(i);
+            Io::read(x);
+            i--;
+
+            unordered_map<int, int> x_factor;
+            update_factors(x_factor, x);
+            for(auto pr : x_factor) {
+                if(counts[pr.first].size() == n) {
+                    g = mod.mul(g, mod.inv(mod.pow(pr.first, *counts[pr.first].begin())));
+                }
+                if(factors[i].find(pr.first) != factors[i].end()) {
+                    counts[pr.first].erase(counts[pr.first].find(factors[i][pr.first]));
+                }
+                factors[i][pr.first] += pr.second;
+                counts[pr.first].insert(factors[i][pr.first]);
+                if(counts[pr.first].size() == n) {
+                    g = mod.mul(g, mod.pow(pr.first, *counts[pr.first].begin()));
+                }
+            }
+
+            Io::write(g);
+        }
+    }
+};
+ 
+void init() {
+    ios::sync_with_stdio(false);
+}
+ 
+int main() {
+    init();
+ 
+    int t = 1;
+    //Io::read(t);
+ 
+    while (t--) {
+        Solver solver;
+        solver.read();
+        solver.solve();
+    }
+    return 0;
+}
