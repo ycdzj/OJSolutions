cc449091a05df5a6bf7c7832898a465caed67a97
ycdzj <yczhao0123@gmail.com>
2021-03-11 20:41:41 +0800

    add codeforces 1494 ABCD

---
+++ cf/contest/1494/d/d.cpp
@@ -0,0 +1,249 @@
+// ----------------------Template Start----------------------
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+using namespace std;
+ 
+class Io {
+public:
+    static void read(int &val) { scanf("%d", &val); }
+    static void read(long long &val) { scanf("%lld", &val); }
+    static void read(char *val) { scanf("%s", val); }
+    static void read(string &val) {
+        const int BUFFER_SIZE = 2e6 + 5;
+        static char buffer[BUFFER_SIZE];
+        scanf("%s", buffer);
+        val = buffer;
+    }
+    static void read(vector<int> &val, int n) {
+        val.resize(n);
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &val[i]);
+        }
+    }
+    static void read(vector<long long> &val, int n) {
+        val.resize(n);
+        for (int i = 0; i < n; i++) {
+            scanf("%lld", &val[i]);
+        }
+    }
+ 
+    static void write(int val) { printf("%d\n", val); }
+    static void write(long long val) { printf("%lld\n", val); }
+    static void write(const char *val) { printf("%s\n", val); }
+    static void write(const string &val) { printf("%s\n", val.c_str()); }
+    static void write(const vector<int> &val) {
+        for (int i = 0; i < val.size(); i++) {
+            if (i > 0) {
+                putchar(' ');
+            }
+            printf("%d", val[i]);
+        }
+        putchar('\n');
+    }
+    static void write(const vector<long long> &val) {
+        for (int i = 0; i < val.size(); i++) {
+            if (i > 0) {
+                putchar(' ');
+            }
+            printf("%lld", val[i]);
+        }
+        putchar('\n');
+    }
+};
+ 
+class Mod {
+public:
+    int mod;
+ 
+    explicit Mod(int mod_) : mod(mod_) {}
+ 
+    int add(int a, int b) const { return (a + b) % mod; }
+    int sub(int a, int b) const { return (a + mod - b) % mod; }
+    int mul(long long a, int b) const { return (a * b) % mod; }
+    int pow(long long a, int x) const {
+        int ans = 1;
+        while (x) {
+            if (x & 1) {
+                ans = (ans * a) % mod;
+            }
+            x >>= 1;
+            a = (a * a) % mod;
+        }
+        return ans;
+    }
+    int inv(int x) const {
+        return pow(x, mod - 2);
+    }
+};
+ 
+class Math {
+public:
+    static long long gcd_ex(long long a, long long b, long long &x, long long &y) {
+        if (b == 0) {
+            x = 1;
+            y = 0;
+            return a;
+        }
+        long long ans = gcd_ex(b, a % b, y, x);
+        y -= (a / b) * x;
+        return ans;
+    }
+};
+ 
+class Combine {
+    vector<int> fac;
+    vector<int> inv_fac;
+    Mod mod;
+ 
+public:
+    explicit Combine(int n, Mod mod_) : fac(n), inv_fac(n), mod(mod_) {
+        fac[0] = 1;
+        for (int i = 1; i < n; i++) {
+            fac[i] = mod.mul(fac[i - 1], i);
+        }
+        inv_fac[n - 1] = mod.inv(fac[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            inv_fac[i] = mod.mul(inv_fac[i + 1], i + 1);
+        }
+    }
+    int arrange(int n, int m) {
+        if (n < m) {
+            return 0;
+        }
+        return mod.mul(fac[n], inv_fac[n - m]);
+    }
+    int combine(int n, int m) {
+        if (n < m) {
+            return 0;
+        }
+        return mod.mul(arrange(n, m), inv_fac[m]);
+    }
+};
+ 
+class Dsu {
+    int n;
+    vector<int> f;
+ 
+public:
+    explicit Dsu(int n_) : n(n_), f(n_) {
+        for(int i = 0; i < f.size(); i++) {
+            f[i] = i;
+        }
+    }
+    int find(int x) {
+        if(f[x] != x) {
+            f[x] = find(f[x]);
+        }
+        return f[x];
+    }
+    void merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        f[x] = y;
+    }
+};
+ 
+// ----------------------Template End----------------------
+class Solver {
+    int n;
+    vector<vector<int>> a;
+
+    vector<vector<int>> adj;
+    vector<int> w;
+
+    void build(const vector<int> indices, int root) {
+        if(indices.empty()) {
+            return;
+        }
+        if(indices.size() == 1) {
+            adj[root].push_back(indices[0]);
+            return;
+        }
+
+        map<int, vector<int>> partation;
+        for(int i = 1; i < indices.size(); i++) {
+            partation[a[indices[0]][indices[i]]].push_back(indices[i]);
+        }
+
+        int pre = indices[0];
+        for(auto pr : partation) {
+            int u = root;
+            if(pr.first != w[root]) {
+                adj.emplace_back();
+                w.push_back(pr.first);
+                u = adj.size() - 1;
+            }
+
+            build(pr.second, u);
+            adj[u].push_back(pre);
+            pre = u;
+        }
+        if(pre != root) {
+            adj[root].push_back(pre);
+        }
+    }
+public:
+    void read() {
+        Io::read(n);
+        a.resize(n);
+        for(int i = 0; i < n; i++) {
+            Io::read(a[i], n);
+        }
+    }
+    void solve() {
+        adj.resize(n);
+        w.resize(n);
+        for(int i = 0; i < n; i++) {
+            w[i] = a[i][i];
+        }
+
+        int max_a = *max_element(a[0].begin(), a[0].end());
+        adj.emplace_back();
+        w.push_back(max_a);
+
+        vector<int> indices(n);
+        for(int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+        build(indices, n);
+
+        Io::write((int)adj.size());
+        Io::write(w);
+        Io::write(n + 1);
+        for(int u = 0; u < adj.size(); u++) {
+            for(int v : adj[u]) {
+                Io::write(vector<int>{v + 1, u + 1});
+            }
+        }
+    }
+};
+ 
+void init() {
+    ios::sync_with_stdio(false);
+}
+ 
+int main() {
+    init();
+ 
+    int t = 1;
+    //Io::read(t);
+ 
+    while (t--) {
+        Solver solver;
+        solver.read();
+        solver.solve();
+    }
+    return 0;
+}
