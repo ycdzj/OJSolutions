8c64af0618e5dbeac80c5fac4e4e70f97da8a662
ycdzj <ycdzj@ycdzjs-MacBook-Pro.local>
2023-03-13 04:46:05 +0000

    add cf_1804

--- cf_1804A.cpp
+++ cf_1804A.cpp
@@ -12,16 +12,27 @@
 using namespace std;
 
 class Solver {
+  int a, b;
  public:
-  void read() {}
+  void read() {
+    cin >> a >> b;
+  }
 
-  void solve() {}
+  void solve() {
+    a = abs(a);
+    b = abs(b);
+    int ans = a + b + abs(a - b);
+    if (abs(a - b) > 0) {
+      ans--;
+    }
+    cout << ans << endl;
+  }
 };
 
 int main() {
   ios::sync_with_stdio(false);
   int t = 1;
-  // cin >> t;
+  cin >> t;
   while (t--) {
     Solver solver;
     solver.read();
